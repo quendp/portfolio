022a9a883541346f22fed8f17eb9dbc3f6fe904b
Quen DP <delpilarquen@gmail.com>
2022-12-27 22:01:58 +0800

    Convert loader.js functions to asynchronous

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -1,71 +1,103 @@
 const loaderWrapper = document.getElementById("loaderWrapper");
-const loaderContainer = document.getElementById("loaderContainer");
 const loaderBefore = document.getElementById("loaderBefore");
 const loaderAfter = document.getElementById("loaderAfter");
 const loaderText = document.getElementById("loaderText");
+const loaderTextSub = document.getElementById("loaderTextSub");
 const loaderBar = document.getElementById("loaderBar");
 const loaderPercent = document.getElementById("loaderPercent");
 const loaderSub = document.getElementById("loaderSub");
 
-
 let percentValue = 1;
 let documentLoaded = false;
-const loadScreen = () => {
-    loaderSub.style.maxWidth = "12.9rem"
-    setTimeout(() => {
-        loaderText.style.opacity = "1";
-        loaderText.style.transform = "translateY(0)";  
-    }, 500);
-    setTimeout(() => {
-        loaderSub.style.opacity = "1";
-    }, 1500);
-    setTimeout(() => {
-        let percentInterval = setInterval(() => {
-            if (percentValue == 100) {
-                clearInterval(percentInterval);
-                if (documentLoaded){
-                    exitLoader();
-                };
-            } else {
-                loaderBar.style.width = percentValue + "%";
-                loaderPercent.textContent = percentValue;
-                percentValue++;
-            };
-        }, 50);
-    }, 2000);
-};
-
-const exitLoader = () => {
-    loaderBar.style.width = "100%";
-    loaderPercent.textContent = "100";
-    loaderText.style.transform = "translateY(5rem)";
-    setTimeout(() => {
-        loaderText.textContent = "Welcome to my portfolio.";
-        loaderText.style.transform = "translateY(0)";
-    }, 500);
-    setTimeout(() => {
-        loaderText.style.transform = "translateY(5rem)";
-    }, 2000);
-    setTimeout(() => {
-        loaderBefore.style.transition = "width 800ms ease-in-out";
-        loaderAfter.style.transition = "width 800ms ease-in-out";
-        loaderBefore.style.width = "50%";
-        loaderAfter.style.width = "50%";
-        loaderWrapper.style.clipPath = "circle(100%)";
-    }, 2500);
-    setTimeout(() => {
-        loaderWrapper.style.transition = "all 1000ms ease-in-out";
-        loaderWrapper.style.clipPath = "circle(3%)";
-    }, 3000);
-    
-};
 
 window.addEventListener("DOMContentLoaded", (event) => {
     documentLoaded = true;
-    console.log(documentLoaded)
-    if (percentValue == 100) {
-        exitLoader();
-    }
 });
 
-loadScreen();
+const isLoaded = () => {
+    let loadInterval = setInterval(() => {
+    if (documentLoaded){
+            exitLoader();
+            clearInterval(loadInterval);
+        } else if (percentValue < 100) {
+            loaderBar.style.width = percentValue + "%";
+            loaderPercent.textContent = percentValue;
+            percentValue++;
+        }
+    }, 500);
+};
+
+const percentAnimation = (interval, limit) => {
+    let percentInterval = setInterval(() => {
+        if (percentValue > limit) {
+            clearInterval(percentInterval);
+        } else {
+            loaderBar.style.width = percentValue + "%";
+            loaderPercent.textContent = percentValue;
+            percentValue++;
+        }
+    }, interval);
+};
+
+function animationDelay(time){
+    return new Promise ((resolve) => {
+        setTimeout(resolve, time);
+    })
+}
+
+async function animateLoader() {
+    try {
+        await animationDelay(500)
+        loaderSub.style.maxWidth = "12.9rem"
+        loaderText.style.opacity = "1"
+        loaderText.style.transform = "translateY(0)"
+
+        await animationDelay(1000)
+        loaderSub.style.opacity = "1"
+
+        await animationDelay(500)
+        percentAnimation(30, 75)
+
+        await animationDelay(2300)
+        isLoaded()
+
+    } catch(err){
+        console.log(err);
+    }
+}
+
+async function exitLoader () {
+    try {
+        await animationDelay(0)
+        percentAnimation(80, 100)
+
+        await animationDelay(2000)
+        loaderText.style.transform = "translateY(5rem)"
+
+        await animationDelay(500)
+        loaderText.textContent = "Welcome to my portfolio."
+        loaderText.style.transform = "translateY(0)"
+
+        await animationDelay(500)
+        loaderSub.style.opacity = "0"
+
+        await animationDelay(500)
+        loaderTextSub.style.overflow = "visible"
+        loaderText.style.transform = "translateY(2rem)"
+
+        await animationDelay(2000)
+        loaderBefore.style.transition = "width 800ms ease-in-out"
+        loaderAfter.style.transition = "width 800ms ease-in-out"
+        loaderBefore.style.width = "50%"
+        loaderAfter.style.width = "50%"
+        loaderWrapper.style.clipPath = "circle(100%)"
+
+        await animationDelay(500)
+        loaderWrapper.style.transition = "all 1000ms ease-in-out"
+        loaderWrapper.style.clipPath = "circle(3%)"
+    } catch(err) {
+        console.log(err)
+    }
+};
+
+animateLoader();
